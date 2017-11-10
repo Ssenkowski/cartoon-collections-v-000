ddde7cb819710bb5e5385def2a91986d7ba1e881
Steven Senkowski <scsenkowski@gmail.com>
2017-11-10 20:07:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-    dwarves.each_with_index {|dwarf, i| puts "#{dwarf}. #{index}"}
+    dwarves.each_with_index {|dwarf, i| puts "#{dwarf}. #{i}"}
 end
 
 def summon_captain_planet# code an argument here
