23406084b7300b155cb9bee036a30edf04be568a
Steven Senkowski <scsenkowski@gmail.com>
2017-11-10 20:27:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses = %w(gouda cheddar camembert)
 
   cheese.find do |cheesey_types|
